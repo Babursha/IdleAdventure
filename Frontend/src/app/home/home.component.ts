a291048067e27b3a4c0e817380963bdaac467ca1
Babursha Saleem <babursha.saleem@gmail.com>
2021-05-24 17:57:02 -0400

    added guidebook to teach player about the game.

--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -26,6 +26,26 @@ export class HomeComponent implements OnInit {
   showGoldCollected = false;
   showXpCollected = false;
   authorized = false;
+  showDetails = false;
+  showDungGuide= false;
+  showItemGuide = false;
+  showBestiaryGuide = false;
+  showCraftGuide = false;
+  showPassGuide = false;
+  showEnchGuide = false;
+  showBatGuide = false;
+  dungeonTip="Battles take place automatically in various different themed areas to acquire loot bags.\n"+
+              "'Regular' Loot bags drop at a 25% chance per monster, 'Boss' Loot bags drop at a 5% chance from bosses.\nOpen the loot bags in the 'Inventory'"+
+              "tab to receive crafting materials assigned to that type of loot bag.\n Battles in dungeon's will continue infinitely until the user clicks on the 'leave' button.\n"+
+              "If 'leave' is clicked during battle, the battle simulations keeps going until the user faints or the monster faints.\n Dying in battle causes you"+
+              " to drop half of your gained resources.";
+
+  craftingTip="Crafted equipment scales based on the crafting materials used.\n Currently potions only have 3 tiers but more will be added.\n Misc crafting "+
+              "planned.\n Unlock achievements by crafting every possible weapon.";
+
+  battleTip ="Idle battle system which fights monsters and collects loot on its own.\nAutomated potion usage with user specified Hp threshold.\n Damage is calculated as every point in attack is negated by a point of defense.\n"+
+             "Critical hits do 1.5x damage at base.\n Evasion and hit chance currently not implemented.\n Once a monster dies a new one will spawn automatically.\n"
+
 
   constructor(private http: HttpClient,private router: Router,public dataService: DataService) { }
 
@@ -63,6 +83,44 @@ export class HomeComponent implements OnInit {
           err=>{
           }
     );
+  }
+  resetInfo(){
+    this.showDetails = false;
+    this.showDungGuide= false;
+    this.showItemGuide = false;
+    this.showBestiaryGuide = false;
+    this.showCraftGuide = false;
+    this.showPassGuide = false;
+    this.showEnchGuide = false;
+    this.showBatGuide = false;
+  }
+  showDungeonGuide(){
+    this.showDetails=true;
+    this.showDungGuide = true;
+  }
+  showCraftingGuide(){
+    this.showDetails=true;
+    this.showCraftGuide = true;
+  }
+  showBattleGuide(){
+    this.showDetails=true;
+    this.showBatGuide = true;
+  }
+  showEnchantGuide(){
+    this.showDetails=true;
+    this.showEnchGuide=true;
+  }
+  showItemIndex(){
+    this.showDetails=true;
+    this.showItemGuide=true;
+  }
+  showBestiary(){
+    this.showDetails=true;
+    this.showBestiaryGuide = true;
+  }
+  showPassiveGuide(){
+    this.showDetails=true;
+    this.showPassGuide = true;
   }
 
   collectGold(){
