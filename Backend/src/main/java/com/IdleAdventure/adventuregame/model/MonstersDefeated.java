f9b9d7379ec6867e03ab1e4fd41e9f1f6e29c3ba
Babursha Saleem <babursha.saleem@gmail.com>
2021-06-21 14:10:38 -0400

    Keeps track of monsters defeated.

---
+++ Backend/src/main/java/com/IdleAdventure/adventuregame/model/MonstersDefeated.java
@@ -0,0 +1,23 @@
+package com.IdleAdventure.adventuregame.model;
+
+public class MonstersDefeated {
+
+    private String area;
+    private int total;
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+}
